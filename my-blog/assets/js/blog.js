6b53a5e15136951bdf0af1c6957f951f246c935e
porti@Portillo17 <portillo_andres17@yahoo.com>
2024-06-11 22:54:43 -0400

    fix and add boxes on blog.js

--- my-blog/assets/js/blog.js
+++ my-blog/assets/js/blog.js
@@ -1,15 +1,29 @@
-// const button = document.getElementById('incrementNum')
-
-
 let number = JSON.parse(localStorage.getItem('saveLastPost'))
 console.log(number)
 
 
 function showPrevious() {
-    console.log(number.username)
-    const usernamePTag=document.createElement("p")
-    usernamePTag.textContent
+    const divTag=document.createElement("div")
+    divTag.setAttribute("class", "card")
+
+    const headTag=document.createElement("div")
+    headTag.setAttribute("class", "card-body")
+
+    const usernamePTag=document.createElement("h5")
+    usernamePTag.textContent = number.username
+    headTag.append(usernamePTag)
+
+    const titlePTag=document.createElement("h4")
+    titlePTag.textContent = number.title
+    headTag.append(titlePTag)
+    
+    const contextPTag=document.createElement("h3")
+    contextPTag.textContent = number.context
+    headTag.append(contextPTag)
+    document.body.append(divTag)
+    divTag.append(headTag)
 }
 
 showPrevious()
 
+divTag.setAttribute
--- my-blog/assets/js/form.js
+++ my-blog/assets/js/form.js
@@ -1,54 +1,33 @@
 // Access toggle switch HTML element
-
-const sun = document.getElementById('sun')
-const themeSwitcher = document.querySelector('#theme-switcher');
-const container = document.querySelector('.container');
-const summit = document.querySelector('#summit')
-
-let mode = 'dark';
-
-// Listen for a click event on toggle switch
-// themeSwitcher.addEventListener('click', function () {
-//   // If mode is dark, apply light background
-//   if (mode === 'dark') {
-//     mode = 'light';
-//     container.setAttribute('class', 'light');
-//   }
-//   // If mode is light, apply dark background
-//   else {
-//     mode = 'dark';
-//     container.setAttribute('class', 'dark');
-//   }
-// });
-
-// // created a function to have the screen light and the 'sun icon' showing 
-// IF (mode === "light") {
-//   then display the SUN icon
-// } 
-
-// // created a function to have the screen dark and the 'moon icon' showing
-// if (mode == "dark") {
-//   then display the MOON icon 
-// }
-
-const username = document.querySelector('#email');
-const title = document.querySelector('#title');
-const context = document.querySelector('#context');
-
-  // Save related form data as an object
-function saveLastPost(){
-if (!username || !title || !context) {
-return; 
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("sun");
 }
+
+// if (localStorage.getItem('saveLastPost')==null){} 
+  // Save related form data as an object
+function saveLastPost(e){
+e.preventDefault()
+console.log(e)
+
+const username = document.getElementById('username');
+const title = document.getElementById('title');
+const context = document.getElementById('context');
+
+  if (!username.value || !title.value || !context.value) {
+  return; 
+  }
+
   const lastPost = {
     username:username.value,
     title:title.value,
     context:context.value,
   }
+  
   console.log(lastPost)
    // Use .setItem() to store object in storage and JSON.stringify to convert it as a string
   localStorage.setItem('saveLastPost', JSON.stringify(lastPost));
   location.href="./blog.html"
 }
 
-summit.addEventListener ("click", saveLastPost )
+summit.addEventListener ("click", saveLastPost)
